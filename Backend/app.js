36c418a6a9778ceb20c2b1351ef58175905ab7de
ayusshh19 <cm.b.49ayush.shukla@gmail.com>
2024-01-06 13:45:07 +0530

    token error resolved

--- Backend/app.js
+++ Backend/app.js
@@ -8,24 +8,24 @@ const corsOption = {
     origin: ['http://localhost:5173'],
     credentials:true, 
 };
-app.use(cors(corsOption));
+// app.use(cors(corsOption));
 app.use(cors())
 const errorMiddleware = require("./middleware/error");
 
 
-app.use(function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', req.headers.origin);
-    res.header('Content-Type', 'application/json;charset=UTF-8')
-    res.header('Access-Control-Allow-Credentials', true)
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    res.header(
-      'Access-Control-Allow-Headers',
-      'Origin, X-Requested-With, Content-Type, Accept'
-    )
-    next()
-  })
+// app.use(function(req, res, next) {
+//   res.header('Access-Control-Allow-Origin', req.headers.origin);
+//     res.header('Content-Type', 'application/json;charset=UTF-8')
+//     res.header('Access-Control-Allow-Credentials', true)
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//     res.header(
+//       'Access-Control-Allow-Headers',
+//       'Origin, X-Requested-With, Content-Type, Accept'
+//     )
+//     next()
+//   })
 
-app.use(bodyParser.json({limit: '50mb'}));
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
 app.use(express.json());
 app.use(cookieParser());
